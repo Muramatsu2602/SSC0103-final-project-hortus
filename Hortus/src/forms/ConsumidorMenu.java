771df85dc568dc673d8301a2f8831a7332860aa2
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-07-14 23:00:59 -0300

    EstoqueProdutorForm is done in frontend

--- Hortus/src/forms/ConsumidorMenu.java
+++ Hortus/src/forms/ConsumidorMenu.java
@@ -71,7 +71,7 @@ public class ConsumidorMenu {
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
-			public void run() {
+			public void run() {  
 				try {
 					ConsumidorMenu window = new ConsumidorMenu();
 					window.frame.setVisible(true);
