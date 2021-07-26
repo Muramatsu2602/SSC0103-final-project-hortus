ba276650ee3333f261e4afebe132ff2a0e0f7da8
giovanni-shibaki <giovanni.shibaki@usp.br>
2021-07-26 17:02:03 -0300

    fix

--- Hortus/src/hortus/Consumidor.java
+++ Hortus/src/hortus/Consumidor.java
@@ -6,11 +6,5 @@ public class Consumidor extends Usuario {
 	public Consumidor(int id, String nome, String cpf, String telefone, Endereco endereco, String email, String senha) {
 		super(id, nome, cpf, telefone, endereco, email, senha);
 	}
-
-	// Retorna FALSE se o ID do produto não existe, ou ocorreu algum erro
-	public void adicionarProdutoFavorito(Produto p) throws HortusException {
-		SGBD sgbd = new SGBD();
-		sgbd.connect();
-		sgbd.insereProdutoFavorito(this, p);			
-	}
+	
 }
